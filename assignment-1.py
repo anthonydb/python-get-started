efac6cb52c8aebb130a58f7db4360580ec89f7cd
adebarros <adebarros@gmail.com>
2014-02-21 22:58:56 -0500

    Add assignment 1.

---
+++ assignment-1.py
@@ -0,0 +1,18 @@
+# Getting Started Assignment 1:
+
+# Given the below list of dictionaries, write code to:
+# -- iterate through the list of dictionaries; 
+# -- for each dictionary, check whether the make is a Honda; 
+# -- if make is Honda, print the color year Honda model
+# -- Precede the list with text intro
+
+# Your output should look like:
+# 
+# Hondas currently in stock include: 
+# Black 2010 Honda Accord
+# Gray 1999 Honda Civic EX
+
+cars = [{'make': 'Toyota', 'year': 2013, 'color': 'Silver', 'model': 'Corolla'}, 
+        {'make': 'Honda', 'year': 2010, 'color': 'Black', 'model': 'Accord'},
+        {'make': 'Ford', 'year': 2001, 'color': 'Blue', 'model': 'Mustang'},
+        {'make': 'Honda', 'year': 1999, 'color': 'Gray', 'model': 'Civic EX'}]
