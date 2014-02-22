9e78b8be69b4891b8699efe37e415b4dc34b98e4
adebarros <adebarros@gmail.com>
2014-02-21 22:44:29 -0500

    Flesh out part 1.

--- 1-variables-data-structures.py
+++ 1-variables-data-structures.py
@@ -6,34 +6,58 @@ a = 1
 print a
 
 # decimal
-pi = 3.14
+pi = 3.14159
 print pi
 
 # string
 guy = 'Fred'
 print guy
 
-# You can concatenate these if you cast as strings
+# Variables of different types can be
+# concatenated if cast as strings
 print guy + ' has ' + str(pi) + ' days to eat ' + str(a) + ' donut.'
 
 
 # Data structures
 # -----------------
 
-# Lists (mutable)
+
+# Lists (are mutable)
+
+# Create a list: 
 car_list = ['Honda', 'Ford', 'Buick']
+
+# Retrieve the first item:
 print car_list[0]
+
+# Retrieve the first two items (slicing)
 print car_list[0:2]
+
+# Add and remove items from the list
 car_list.append('Kia')
 car_list.remove('Honda')
 print car_list
 
-# Tuples (immutable)
+
+
+# Tuples (are immutable)
+
+# Create a tuple
 cars_tuple = ('Honda', 'Ford', 'Buick')
+
+# Retrieve the third item (note zero index)
 print cars_tuple[2]
 
+
+
 # Dictionaries
+
+# Create a dictionary
 cars_dict = {'make': 'Honda', 'year': 2010, 'color': 'Silver'}
+
+# Retrieve the value for the key 'year'
 print cars_dict['year']
+
+# Set a new value for the key 'year'
 cars_dict['year'] = 2013
 print cars_dict['year']
