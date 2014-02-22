f6d613376bf850327952a257122dc0bfc627a668
adebarros <adebarros@gmail.com>
2014-02-21 22:52:48 -0500

    Add string examples.

--- 2-string-methods.py
+++ 2-string-methods.py
@@ -1,19 +1,37 @@
 # STRING METHODS
 # http://docs.python.org/2/library/stdtypes.html#string-methods
 
+
 # slicing strings
+#------------------------
+
 phone = '703-555-1212'
 print phone[0:3]
 print phone[4:7]
 print phone[8:]
 
+
+# modifying strings
+#-------------------------
+
 a = 'Happy Birthday, Bob!'
 print a
+
+# how long is the string?
 print len(a)
+
+# lowercase the string
 print a.lower()
+
+# replace Bob with Sue
 print a.replace('Bob', 'Sue')
+
+# chain replacements
 print a.replace('Bob', 'Sue').replace('!', '')
+
+# split the string on spaces
 print a.split()
 
+# split a strong on commas
 b = 'Bob,Sue,,Mary,Steve'
 print b.split(',')
