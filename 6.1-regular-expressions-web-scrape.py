c5acc4638e683410525b20655c506179a55148b0
adebarros <adebarros@gmail.com>
2014-02-22 09:11:22 -0500

    Add some regex stuff.

---
+++ 6.1-regular-expressions-web-scrape.py
@@ -0,0 +1,55 @@
+##########
+HTML example
+
+<strong>Licensed MWt: </strong> 3,438 <br />
+<strong>Licensed MWt:</strong> 2,568<br />
+
+
+
+
+########
+Fetch and print web page contents
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+
+main_url = 'http://www.nrc.gov/reactors/operating/list-power-reactor-units.html'
+base_url = 'http://www.nrc.gov'
+
+r = requests.get(main_url)
+html_soup = BeautifulSoup(r.text)
+
+#output the columns as a CSV
+    
+csvfile = open('reactors.csv', 'w')
+csvwriter = csv.writer(csvfile, delimiter = ',')
+
+headers = ('LINK', 'PLANT NAME', 'REACTOR TYPE', 'LOCATION', 'OWNER', 'NRC REGION', 'LICENSED MWt')
+csvwriter.writerow(headers)
+
+table = html_soup.find('table')
+for row in table.find_all('tr')[1:]:
+    col = row.find_all('td')
+    link = col[0].find('a').get('href') # link
+    reactor_name = col[0].find('a').contents[0] # name
+    reactor_type = col[1].string
+    location = col[2].string.encode('Latin')
+    owner_scrape = col[3].string
+    if owner_scrape == None:
+        owner = ''
+    else:
+        owner = col[3].string.encode('Latin')
+    region = col[4].string
+
+    print 'Following ' + link
+    r_follow = requests.get(base_url + link)
+    mega_watts = re.search('Licensed (MWt: |MWt:)</strong>(.*)<br />', r_follow.text)
+
+    parsed_row = (
+        link, reactor_name, reactor_type, location, owner, region, mega_watts.group(2)
+        )
+
+    csvwriter.writerow(parsed_row)
+
+csvfile.close()
