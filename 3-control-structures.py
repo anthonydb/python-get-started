2fe33a7ba9356bf33f80e0cfeadabf3110b440f2
adebarros <adebarros@gmail.com>
2014-02-21 23:14:30 -0500

    Start on control structures.

---
+++ 3-control-structures.py
@@ -0,0 +1,24 @@
+# Control structures 
+
+# if ... then ... else
+vote = 'No'
+
+if vote == 'Yes':
+    print 'The vote is Yes'
+elif vote == 'No':
+    print 'The vote is No'
+else:
+    print 'It\'s some other vote'
+
+
+# while
+count = 10
+while (count >= 0):
+   print 'The count is: ' + str(count)
+   count = count - 1
+print 'Liftoff!'
+
+# for loop
+car_list = ['Honda', 'Ford', 'Buick']
+for car in car_list:
+    print car
