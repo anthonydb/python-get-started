3002028664e27f7941651a8e8ae4fd56babd93bb
Anthony DeBarros <adebarros@gmail.com>
2014-12-29 20:50:46 -0500

    PEP8 cleanup and fix for extra lines in CSV output.

--- 6.1-regular-expressions-web-scrape.py
+++ 6.1-regular-expressions-web-scrape.py
@@ -2,8 +2,6 @@
 # We're looking at the U.S. Nuclear Regulatory Commission
 # list of power reactor units
 
-
-
 import re
 import csv
 import requests
@@ -17,23 +15,24 @@ r = requests.get(main_url)
 html_soup = BeautifulSoup(r.text)
 
 # output the columns as a CSV
-csvfile = open('reactors.csv', 'w')
-csvwriter = csv.writer(csvfile, delimiter = ',')
+csvfile = open('reactors.csv', 'wb')
+csvwriter = csv.writer(csvfile, delimiter=',')
 
 # create headers for the CSV
-headers = ('LINK', 'PLANT NAME', 'REACTOR TYPE', 'LOCATION', 'OWNER', 'NRC REGION', 'LICENSED MWt')
+headers = ('LINK', 'PLANT NAME', 'REACTOR TYPE', 'LOCATION',
+           'OWNER', 'NRC REGION', 'LICENSED MWt')
 csvwriter.writerow(headers)
 
 # find the table and extract the columns from each row
 table = html_soup.find('table')
 for row in table.find_all('tr')[1:]:
     col = row.find_all('td')
-    link = col[0].find('a').get('href') # link to detail page
-    reactor_name = col[0].find('a').contents[0] # name
+    link = col[0].find('a').get('href')  # link to detail page
+    reactor_name = col[0].find('a').contents[0]  # name
     reactor_type = col[1].string
-    location = col[2].string.encode('Latin') # encoding to fix character issues
+    location = col[2].string.encode('Latin')  # encoding to fix character issues
     owner_scrape = col[3].string
-    if owner_scrape == None:
+    if owner_scrape is None:
         owner = ''
     else:
         owner = col[3].string.encode('Latin')
@@ -47,9 +46,8 @@ for row in table.find_all('tr')[1:]:
     mega_watts = re.search('Licensed (MWt: |MWt:)</strong>(.*)<br />', r_follow.text)
 
     # a tuple for the final parsed row
-    parsed_row = (
-        link, reactor_name, reactor_type, location, owner, region, mega_watts.group(2)
-        )
+    parsed_row = (link, reactor_name, reactor_type, location,
+                  owner, region, mega_watts.group(2))
 
     csvwriter.writerow(parsed_row)
 
